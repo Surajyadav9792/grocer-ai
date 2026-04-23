2da57c0fac8a6cf52744e73a688ad56f6b9ee52b
Suraj Yadav <surajyadav2823510@gmail.com>
2026-04-23 12:24:59 +0530

    feat: implement cached mongoose connection to prevent multiple connections

--- grocer-ai/src/utils/db.ts
+++ grocer-ai/src/utils/db.ts
@@ -5,4 +5,26 @@ import mongoose from "mongoose";
  }
 
  let cached=global.mongoose;
- 
+ //if in the cache the mongoose obj is not prsent then we mark or store null in  conn and promise variable which is define in global.d.ts 
+ if(!cached){
+    cached=global.mongoose={conn:null,promise:null}
+ }
+ const connectDb=async()=>{
+    //if connection is present in the catch then return the connect 
+    if(cached.conn){
+        return cached.conn;
+    }
+    if(!cached.promise){
+        cached.promise=mongoose.connect(MongoConectionString).then((conn)=>conn.connection) //  through mongoose.connect we got a string but we need a promise so  by .then((conn)=>conn.connection)  we get a promise which is go for resolve
+    }
+    // if not present then may be have promise and here we resolve the promise 
+    try{
+        const conn=await cached.promise
+        return conn
+    }
+    catch(error){
+        console.log(error);
+    }
+
+ }
+ export default connectDb;
